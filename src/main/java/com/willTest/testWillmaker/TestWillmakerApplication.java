a2d73b004d220d85d9e5c08c1f488698272f70ed
Kaigh <krazykaigh@users.noreply.github.com>
2019-09-15 15:39:32 -0600

    WillMaker | Added Spring Initializer files
    Project Metadata
    Group: com.willTest
    Artifact: testWillmaker
    Options
    Name: testWillmaker
    Description: Spring Boot Will Maker Program
    Package Name: com.willTest.testWillmaker

---
+++ src/main/java/com/willTest/testWillmaker/TestWillmakerApplication.java
@@ -0,0 +1,13 @@
+package com.willTest.testWillmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWillmakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestWillmakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/willTest/testWillmaker/TestWillmakerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.willTest.testWillmaker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestWillmakerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
